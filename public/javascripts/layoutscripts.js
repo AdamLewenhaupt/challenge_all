b8b4065d63f98a50ef6b56d57c57b9eba468fc56
Adam Lewenhaupt <adam.lewenhauptt@gmail.com>
2012-09-05 17:54:24 +0200

    almost done with layout

--- public/javascripts/layoutscripts.js
+++ public/javascripts/layoutscripts.js
@@ -4,4 +4,6 @@ $(document).ready(function(){
 		width 		= $window.width() - ($sidebar.width() + 30);
 
 	$("#main-frame").width(width).css("left", $sidebar.width() + 20);
+	$("#sub-frame").css("top", 20 + $sidebar.height() + $sidebar.position().top)
+					.width($window.width() - 20);
 });
