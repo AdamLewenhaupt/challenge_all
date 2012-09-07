90abed1a55616cd222cd5b65295648576baddda7
Adam Lewenhaupt <adam.lewenhauptt@gmail.com>
2012-09-07 15:01:03 +0200

    adding and removing fully functional

--- models/schemas.js
+++ models/schemas.js
@@ -9,8 +9,8 @@ var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
 
 exports.User = mongoose.model('User',new Schema({
-    fname: {type: String, required: true},
-    lname: {type: String, required: true},
-    tag: {type: String, required: true, unique: true},
+    fname: {type: String, required: true, lowercase: true, trim: true},
+    lname: {type: String, required: true, lowercase: true, trim: true},
+    tag: {type: String, required: true, unique: true, lowercase: true, trim: true},
     age: {type: Number, required: true}
 }));
