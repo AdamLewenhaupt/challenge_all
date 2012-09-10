8be420f048cf0c8ac3bf7d603e32ed0281e67773
libra guest <adam.lewenhauptt@gmail.com>
2012-09-10 06:43:27 -0400

    working on sse

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ var express = require('express'),
     routes = require('./routes'),
     config = require('./config'),
     http = require('http'),
-    models = require('./models');
+    models = require('./models'),
+    redis = require('redis');
 
 models.db.connect();
 
@@ -18,12 +19,49 @@ var app = express();
 config.app = app;
 
 app.configure(config.dev);
+var publisherClient = redis.createClient(process.env.PORT, process.env.IP);
 
 //Gets
 app.get('/', routes.index);
 app.get('/cs_testing', routes.cs_testing);
 app.get('/ss_testing', routes.ss_testing);
 app.get('/ss_testing/profiles', routes.ss_testing_profiles);
+app.get('/update-stream', function(req, res){
+    req.socket.setTimeout(Infinity);
+    var messageCount = 0;
+    var subscriber = redis.createClient();
+    subscriber.subscribe('updates');
+    
+    subscriber.on('error', function(err){
+        console.log("SSE error: " + err);
+    })
+    
+    subscriber.on('message', function(channel, message){
+        messageCount++;
+        res.write('id: ' + messageCount + "\n");
+        res.write('data: ' + message + "\n\n");
+    });
+    
+    res.writeHead(200, {
+        "Content-Type": "text/eventstream",
+        "Cache-Control": "no-cache",
+        "Connection": "keep-alive"
+    });
+    
+    res.write('\n');
+    
+    req.on('close', function(){
+        subscriber.unsubscribe();
+        subscriber.quit();
+    });
+});
+
+app.get('/fire-event/:eventName', function(req, res){
+    publisherClient.publish('updates', '"' + req.params.eventName + '" page visited');
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write('All clients has received: ' + req.params.eventName);
+    res.end();
+});
 
 //Posts
 app.post('/ss_testing/profiles', routes.ss_testing_create_profile);
