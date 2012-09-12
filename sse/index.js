e6e86d9d35697914f8ccccb02e81874d535b3f1e
libra guest <adam.lewenhauptt@gmail.com>
2012-09-12 04:49:12 -0400

    saveing before trying to fix disconnect error

--- sse/index.js
+++ sse/index.js
@@ -10,25 +10,30 @@ var connections = [];
 var connectionIds = {};
 
 exports.eventStream =  function(req, res){
-    res.id = req.params.id;
     
-    res.writeHead(200, {
-        "content-type": "text/event-stream",
-        "cache-control": "no-cache",
-        "connection": "keep-alive"
-    });
-    
-    res.write('id\n\n');
-    
-    connectionIds[res.id] = connections.length;
-    connections.push(res);
-    console.log("Established connection to: '" +  req.params.id + "'");
-    console.log("Connections: " + connections.length);
-    
-    req.on('close', function(){
+    if(req.headers.accept == "text/event-stream"){
+        res.id = req.params.id;
+        
+        res.writeHead(200, {
+            "content-type": "text/event-stream",
+            "cache-control": "no-cache",
+            "connection": "keep-alive"
+        });
+        
+        res.write('id\n\n');
+        
+        connectionIds[res.id] = connections.length;
+        connections.push(res);
+        console.log("Established connection to: '" +  req.params.id + "'");
+        console.log("Connections: " + connections.length);
+        
+        req.on('close', function(){
+            removeConnection(res);
+        });    
+    }else{
+        res.write("Error: your browser does not support Server-side events");
         res.end();
-        removeConnection(res);
-    });
+    }
 }
 
 function removeConnection(res){
@@ -37,6 +42,7 @@ function removeConnection(res){
     delete connectionIds[res.id];
     console.log("Connection to '" + res.id + "' terminated");
     console.log("Connections:" + connections.length);
+    res.end();
 }
 
 function sseMessage(res, event, data){
