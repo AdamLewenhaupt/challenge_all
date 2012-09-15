c3f13b99e7fcb73eb782816aa3b1e90ae2553c66
Adam Lewenhaupt <adam.lewenhauptt@gmail.com>
2012-09-15 23:45:44 +0200

    edited from sub-frame to newsfeed-frame

--- public/javascripts/layout_default.js
+++ public/javascripts/layout_default.js
@@ -13,10 +13,10 @@ $(document).ready(function(){
 
 	$("#main-frame").width(width).css("left", $sidebar.width() + 15);
 
-	$("#sub-frame").css("top", $sidebar.height() + $sidebar.position().top + 10)
+	$("#newsfeed-frame").css("top", $sidebar.height() + $sidebar.position().top + 10)
 					.width($window.width() - 35 );
 
-	$("#footer").css("top", $("#sub-frame").position().top
-			+ $("#sub-frame").height() + 10)
-					.width($("#sub-frame").width());
+	$("#footer").css("top", $("#newsfeed-frame").position().top
+			+ $("#newsfeed-frame").height() + 10)
+					.width($("#newsfeed-frame").width());
 });
