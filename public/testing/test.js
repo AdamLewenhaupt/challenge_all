bf74b3dd9be7bb512ad8f679d624c228d3be07cb
simpelito <simonciesluk@gmail.com>
2012-09-16 18:19:31 +0200

    saving

--- public/testing/test.js
+++ public/testing/test.js
@@ -1,14 +1,52 @@
 $(document).ready(function(){
 
-function makeButton()
-{
-    $new = $("<button/>");
-    $new.html("A button");
-    $new.click(makeButton);
-    $("#content").append($new);
+$("#container").css({position:"absolute",left:"10px",top:"50px"});
+
+var num = 0,
+    width = 60,
+    height = 60,
+    xOffset = 5,
+    yOffset = 5;
+    row = 0;
+    color = 0;
+
+var buttonlist = [];
+
+function makeButton()	
+{   
+    $new = $("<div/>");
+    if(num==5){
+        num = 0;
+        row++;
+    }
+    if(color==0)$new.css("background","blue");
+    if(color==1)$new.css("background","green");
+    if(color==2)$new.css("background","yellow");
+    if(color==3)$new.css("background","red");
+    $new.css({position:"absolute",width:"60px",height:"60px",
+        left:num*(width+xOffset),top:row*(height+yOffset)});
+    $new.click(removeButton);
+    $new.animate({
+        top: '+=50'
+    }, 400);
+    buttonlist.push($new);
+    $("#container").html(buttonlist);
+    num++;
+    if(color<3)color++;
+    else color=0;
 }
 
-$(".maker").click(function(){
+function removeButton()
+{
+    alert("HEJ");
+}
+
+$("#maker").click(function(){
     makeButton();
 });
+
+$("#flip").click(function(){
+    buttonlist.reverse();
+});
+
 });
