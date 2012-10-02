f629caba379c2a4f3f1e2eedf9af5594d5fbee00
libra guest <adam.lewenhauptt@gmail.com>
2012-10-02 08:38:52 -0400

    added password and email fields to user scheme

--- models/schemas.js
+++ models/schemas.js
@@ -12,5 +12,7 @@ exports.User = mongoose.model('User',new Schema({
     fname: {type: String, required: true, lowercase: true, trim: true},
     lname: {type: String, required: true, lowercase: true, trim: true},
     tag: {type: String, required: true, unique: true, lowercase: true, trim: true},
-    age: {type: Number, required: true}
+    age: {type: Number, required: true},
+    email: {type: String, required: true, lowercase: true, trim: true, unique: true},
+    password: {type: String, required: true}
 }));
