3df90d4766e52fd9137092d9f79873b4279ba0fb
Adam Lewenhaupt <Adam@Adam-Comp.(none)>
2012-10-03 20:37:35 +0200

    fixed form2json error in popup

--- public/js/layout_default.js
+++ public/js/layout_default.js
@@ -5,9 +5,67 @@ Description:
 Std functions on load of every webpage.
 */
 
-function getSSV(){
-	return $("#ssv").html();
+function form2JSON(form){
+    var retval = {},
+        vals = form.children("input")
+        .map(function(){ 
+            return [$(this).attr("name"), $(this).val()];
+             });
+
+    for(i = 0; i < vals.length; i+=2){
+        retval[vals[i]] = vals[i+1];
+    }
+    return retval;
 }
+
+function hasSSV(query){
+    if(!_ssv){
+        var _ssv = $("#ssv").html().split('|');
+    }
+	return _ssv.indexOf(query) != -1;
+}
+
+function popup(options){
+    var width = 400,
+        height = 600,
+        $popup = $("<div/>").addClass("popup"),
+        $form = $("<form/>"),
+        $submit = $("<button/>").addClass("submit"),
+        $cancel = $("<button/>").addClass("cancel"),
+        template = _.template("<%= value %>: <input name='<%= name %>' type='<%= type %>'/>");
+
+    $form.html(_.map(options.inputs, function(input){
+        return template(input);
+    }).join('<br/>')).css({
+        margin: 10
+    });
+
+    $submit.html(options.submit).click(function(){
+        options.success(form2JSON($form));
+        $popup.remove();
+    });
+
+    $cancel.click(false, function(){
+        $popup.remove();
+    })
+
+    $popup.css({
+        position: "fixed",
+        width: width,
+        height: height,
+        "background-color": "gray",
+        "text-align": "center",
+        left: ($(document).width() - width) / 2,
+        top: ($(document).height() - height) / 2
+    }).append($form).append($submit);
+
+    if(options.canCancel){
+        $popup.append($cancel);
+    }
+
+    $(document.body).append($popup);
+}
+
 
 $(document).ready(function(){
 
@@ -26,7 +84,21 @@ $(document).ready(function(){
 			+ $("#sub-frame").height() + 10)
 					.width($("#sub-frame").width());
 
-	if(getSSV().indexOf("req_login") != -1){
-		alert("Require login!");
-	}
+	 if(hasSSV("req_login")){
+        popup({
+            title: "Login",
+            canCancel: false,
+
+            inputs: [
+                { name: "name", type: "text", value: "Username" },
+                { name: "pass", type: "password", value: "Password"}
+            ],
+
+            submit: "Login",
+            success: function(e){
+                console.log(e.data);
+            }
+
+        });
+	} 
 });
