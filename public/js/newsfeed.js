dfe34d21fcf1d137d1007bad520ab8523771fc71
simpelito <simonciesluk@gmail.com>
2012-10-09 23:09:01 +0200

    Working on queue

--- public/js/newsfeed.js
+++ public/js/newsfeed.js
@@ -5,10 +5,13 @@ Description:
 Newsfeed function
 */
 define(["jquery"], function($){
-	var $newsfeed 	= $("#newsfeed");
+	var $newsfeed 	  = $("#newsfeed");
+		newsfeedQueue = new Array();
+
 	function showcase(text){
+		newsfeedQueue.push(text);
 		$new = $("<div style='position: relative; display: inline-block' />");
-		$newsfeed.append($new.html(text));
+		$newsfeed.append($new.html(newsfeedQueue[0]));
 		$new.css({
 				font: '80px"Lucida Grande",Helvetica,Arial,sans-serif',
 				"line-height": '150px',
@@ -19,13 +22,13 @@ define(["jquery"], function($){
 			left: '+='+($newsfeed.width() +$new.width())
 		}, 5000, function() {
 	    // Animation complete.
+	    if(newsfeedQueue.length < 5){
+	    	
+	    	}
+	    	//check if there is a more news or if repeat
+	    }
 	  });
-
-	function queuePush(name,element){
-
-	}
 }
-
-showcase("Hello");
-
+	showcase("Hello");
+	showcase("you");
 });
