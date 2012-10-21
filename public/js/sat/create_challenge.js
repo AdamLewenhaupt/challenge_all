79812d811308a003665908f59d45ce7d0c45239a
Simon Ciesluk <simonciesluk@gmail.com>
2012-10-21 22:21:57 +0200

    Almost done with layout

--- public/js/sat/create_challenge.js
+++ public/js/sat/create_challenge.js
@@ -1,4 +1,21 @@
 define(function(){
 	return function(){
+		$( "#from" ).datepicker({
+            defaultDate: "+1w",
+            changeMonth: true,
+            numberOfMonths: 1,
+            onSelect: function( selectedDate ) {
+                $( "#to" ).datepicker( "option", "minDate", selectedDate );
+            }
+        });
+        $( "#to" ).datepicker({
+            defaultDate: "+1w",
+            changeMonth: true,
+            numberOfMonths: 1,
+            onSelect: function( selectedDate ) {
+                $( "#from" ).datepicker( "option", "maxDate", selectedDate );
+            }
+        });
+        $( "#friendBox" ).selectable();
 	};
 });
