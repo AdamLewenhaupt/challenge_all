96e2c2b926f6bac51154a4fb7d20950caafdd818
= <=>
2012-11-09 14:54:23 +0100

    generalized remove event api

--- routes/events.js
+++ routes/events.js
@@ -3,8 +3,7 @@ var models = require('../../models'),
     Event = models.schemas.Event;
 
 exports.remove = function(req, res){
-	var tag = req.body.tag;
-	Event.remove({user: tag, name: "friend-request"}, function(err){
+	Event.remove(req.body, function(err){
 		if(err) res.send(err);
 	});
 	res.send(success);
