ca38c53e8d815cbabe4750ec8a17a3bb3c8c34db
Simon ciesluk <10gasicie@srgam-ws-010-t3.fryshuset.local>
2013-03-15 15:48:48 +0100

    created retrieve func

--- public/js/sat/challenges.js
+++ public/js/sat/challenges.js
@@ -1,4 +1,7 @@
 define(["jquery"], function($){
+	function retrieve(){
+			return({/*all info*/})
+		}
 	return function(){
 		var $mainframe = $("#main-frame");
 		$mainframe.find(".challenge-display li div").button();
