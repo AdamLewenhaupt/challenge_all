88c6ab0d17f9f399485f923e3f427074bd68c547
Simon ciesluk <10gasicie@srgam-ws-010-t3.fryshuset.local>
2013-03-18 15:48:03 +0100

    walla

--- public/js/sat/challenges.js
+++ public/js/sat/challenges.js
@@ -1,9 +1,36 @@
 define(["jquery"], function($){
-	function retrieve(){
-			return({/*all info*/})
+	function retrieve(challengeID){
+			return({
+				name: "Test name",
+				description: "Best shit ever",
+				rules: ["brinn"],
+				users: ["simpelito"],
+				isPublic: true,
+				date: "",
+				achievements: { 
+					name: "",
+					image: "",
+					color: "",
+					description: ""
+				},
+				hosts: hosts
+			});
 		}
+
+	function displayChallenge(challengeID){
+		$("#challenges-name").animate({opacity: 0}, { duration: 500, queue: false, complete: function(){
+                var result = retrieve(challengeID.name);
+
+                $("#challenges-name").html(result).animate({opacity: 1}, { duration: 500, queue: false });
+            } });
+	}
 	return function(){
 		var $mainframe = $("#main-frame");
 		$mainframe.find(".challenge-display li div").button();
+
+		$(".challenge-display li").click(function(){
+			var found= new object();
+            displayChallenge(found);
+            });
 	};
 });
